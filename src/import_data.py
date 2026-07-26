6642661c06b622f29e06527ac58c8ebe66729409
Tharunadithya133 <anushvivin@gmail.com>
2026-07-26 20:11:35 +0530

    imported olsit dataset to postgre sql with the help of python pipeline

---
+++ src/import_data.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+DB_USER = "postgres"
+DB_PASSWORD = "@Anush77."
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "retail_analytics"
+
+engine = create_engine(
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+data_folder = os.path.join(project_root, "data", "raw")
+
+for file in os.listdir(data_folder):
+    if file.endswith(".csv"):
+        table_name = file.replace(".csv", "").lower()
+
+        file_path = os.path.join(data_folder, file)
+        df = pd.read_csv(file_path)
+
+        df.to_sql(
+            table_name,
+            engine,
+            if_exists="replace",
+            index=False
+        )
+
+        print(f"Imported {file} → {table_name}")
+
+print("All datasets imported successfully!")
