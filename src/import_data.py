ef046adc6b9346666d4a37ca0d6fddb5e8796115
Tharunadithya133 <anushvivin@gmail.com>
2026-07-30 15:38:23 +0530

    Added files under sql to explore, clean, view and for kpi queries

--- src/import_data.py
+++ src/import_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 DB_USER = "postgres"
-DB_PASSWORD = "@Anush77."
+DB_PASSWORD = "Anush77."
 DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "retail_analytics"
@@ -12,10 +12,16 @@ engine = create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
+
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 data_folder = os.path.join(project_root, "data", "raw")
 
+print("Project Root:", project_root)
+print("Data Folder:", data_folder)
+print("Files:", os.listdir(data_folder))
+
 for file in os.listdir(data_folder):
+    print(file)
     if file.endswith(".csv"):
         table_name = file.replace(".csv", "").lower()
 
